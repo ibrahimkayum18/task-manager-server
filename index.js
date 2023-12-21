dea7a7d56da582fa11e013539fdfd3d38065df3f
ibrahimkayum18 <ibrahimkayum017@gmail.com>
2023-12-22 03:01:41 +0600

    Projects post and get api added

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.pqcfxjd.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const projectCollection = client.db('TaskManager').collection('projects')
+
+    app.post('/projects', async(req, res) => {
+        const data = req.body;
+        const result = await projectCollection.insertOne(data);
+        res.send(result)
+    })
+
+    app.get('/projects', async(req, res) => {
+        const result = await projectCollection.find().toArray()
+        res.send(result);
+    })
+
+    // app.get('/projects/:id', async(req, res) => {
+    //     const id = req.params.id;
+    //     const query = {_id: new ObjectId(id)}
+    //     const result = await projectCollection.find(query)
+    //     res.send(result)
+    // })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Task Management Server is running')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
